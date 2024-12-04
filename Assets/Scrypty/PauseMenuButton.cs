f8fca844fb2da2108ae589223a4e7ef09cb39547
Krzys2002 <krzys.sawicki02@gmail.com>
2024-12-04 20:50:32 +0100

    0.0.6-M1-menu

---
+++ Assets/Scrypty/PauseMenuButton.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine;  
+using System.Collections;  
+using UnityEngine.EventSystems;  
+using UnityEngine.UI;
+
+public class PauseMenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Color hoverColor = Color.red; // Color to change to when hovered
+    private Color originalColor; // Original color of the text
+    public TextMeshProUGUI textComponent;
+
+    void Start()
+    {
+        if (textComponent != null)
+        {
+            originalColor = textComponent.color;
+        }
+        else
+        {
+            Debug.LogError("Text component not found.");
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (textComponent != null)
+        {
+            textComponent.color = hoverColor;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (textComponent != null)
+        {
+            textComponent.color = originalColor;
+        }
+    }
+}
+
